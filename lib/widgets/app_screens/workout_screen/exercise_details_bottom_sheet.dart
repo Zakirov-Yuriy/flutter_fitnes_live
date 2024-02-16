7b6535b59058848bb94fd8cf92f7d8710a711170
Zakirov Yuriy <zakcoyote@gmail.com>
2024-02-16 16:09:49 +0300

    video_player_controls

--- lib/widgets/app_screens/workout_screen/exercise_details_bottom_sheet.dart
+++ lib/widgets/app_screens/workout_screen/exercise_details_bottom_sheet.dart
@@ -1,7 +1,6 @@
-import 'dart:async';
-
 import 'package:flutter/material.dart';
 import 'package:video_player/video_player.dart';
+import 'video_player_controls/video_player_controls.dart';
 
 class ExerciseDetailsBottomSheet extends StatefulWidget {
   final String videoUrl; // добавляем URL для видео
@@ -121,7 +120,9 @@ class _ExerciseDetailsBottomSheetState
                         ],
                       ),
                     )
-                  : const CircularProgressIndicator(), // Показываем индикатор загрузки, пока видео не загрузится
+                  : const CircularProgressIndicator(
+                      color: Color.fromRGBO(255, 51, 119, 1),
+                    ), // Показываем индикатор загрузки, пока видео не загрузится
             Padding(
               padding: const EdgeInsets.only(bottom: 10, top: 16),
               child: Row(
@@ -189,89 +190,6 @@ class _ExerciseDetailsBottomSheetState
           ],
         ),
       ),
-    );
-  }
-}
-
-class VideoPlayerControls extends StatefulWidget {
-  final VideoPlayerController controller;
-
-  const VideoPlayerControls({Key? key, required this.controller})
-      : super(key: key);
-
-  @override
-  _VideoPlayerControlsState createState() => _VideoPlayerControlsState();
-}
-
-class _VideoPlayerControlsState extends State<VideoPlayerControls> {
-  late Timer _timer;
-  bool _hideControls = false;
-
-  @override
-  void initState() {
-    super.initState();
-    _timer = Timer.periodic(Duration(seconds: 3), (Timer timer) {
-      if (!_hideControls) {
-        setState(() {
-          _hideControls = true;
-        });
-      }
-    });
-
-    widget.controller.addListener(() {
-      if (widget.controller.value.isPlaying) {
-        _timer.cancel(); // Останавливаем таймер при воспроизведении
-      }
-    });
-  }
-
-  @override
-  void dispose() {
-    _timer.cancel();
-    super.dispose();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Stack(
-      children: <Widget>[
-        GestureDetector(
-          onTap: () {
-            setState(() {
-              _hideControls = !_hideControls;
-            });
-          },
-          child: AbsorbPointer(
-            absorbing: _hideControls,
-            child: AnimatedOpacity(
-              opacity: _hideControls ? 0.0 : 1.0,
-              duration: Duration(milliseconds: 300),
-              child: Container(
-                color: Colors.transparent,
-                child: Center(
-                  child: Icon(
-                    Icons.play_arrow,
-                    color: Color.fromRGBO(255, 51, 119, 1),
-                    size: 100.0,
-                  ),
-                ),
-              ),
-            ),
-          ),
-        ),
-        GestureDetector(
-          onTap: () {
-            setState(() {
-              _hideControls = !_hideControls;
-            });
-            if (widget.controller.value.isPlaying) {
-              widget.controller.pause();
-            } else {
-              widget.controller.play();
-            }
-          },
-        ),
-      ],
     );
   }
 }
